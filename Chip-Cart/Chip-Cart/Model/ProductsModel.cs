a0b7e02c3e00fd57ee35094a0610157ba8fc1985
snehaltate <95269736+snehaltate@users.noreply.github.com>
2022-02-26 16:00:18 +0530

    Add files via upload

---
+++ Chip-Cart/Chip-Cart/Model/ProductsModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chip_Cart.Model
+{
+    public class ProductsModel
+    {
+     [Key]
+        public int id { get; set; }
+        public string title { get; set; } //title, description, categories, price, image,
+        public string description { get; set; }
+        public string categories { get; set; }
+        public int price { get; set; }
+        public string image { get; set; }
+        public DateTime createdAt { get; set; }
+        public DateTime updatedAt { get; set; }
+        public int userid { get; set; }
+
+
+    }
+}
